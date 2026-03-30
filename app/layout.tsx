48e18a1a784f343a2b08824b26aef34c5ee8444d
YuransiS <yura3zaxar@outlook.com>
2026-03-30 14:48:36 +0300

    Update_Pixel_ID_to_1497146881743265

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1963866740868835');
+              fbq('init', '1497146881743265');
               fbq('track', 'PageView');
             `,
           }}
@@ -50,7 +50,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1963866740868835&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1497146881743265&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
